76ff59d4c618cc145f7b3d7dfd2a0f50488dd4cd
RidDevs <127176942+RidDevs@users.noreply.github.com>
2025-03-31 10:01:23 +0530

    auth commit

--- quiz_auth.js
+++ quiz_auth.js
@@ -3,6 +3,7 @@ import { auth, onAuthStateChanged } from './firebase-config.js';
 onAuthStateChanged(auth, (user) => {
     if (user) {
         console.log("User is signed in:", user);
+        window.user = user;
     } else {
         console.log("User is signed out.");
         window.location.href = 'login.html'; 
--- script2.js
+++ script2.js
@@ -1,3 +1,17 @@
+import { auth, onAuthStateChanged } from './firebase-config.js';
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      
+        console.log("User is signed in:", user);
+     
+        const userID = user.uid;
+        console.log(userID);
+    } else {
+        
+        console.log("User is signed out.");
+        window.location.href = 'login.html'; 
+    }
+});
 document.addEventListener('DOMContentLoaded', function() {
         localStorage.removeItem('skillsData');
     const dropZone = document.getElementById('dropZone');
